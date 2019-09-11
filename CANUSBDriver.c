cacbd1a59e9b6e34dfa6a05ea6b6957ec4843d5d
Shymaxtic <shymaxtic@gmail.com>
2019-09-11 20:43:36 +0700

    add function

--- CANUSBDriver.c
+++ CANUSBDriver.c
@@ -1,7 +1,34 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/usb.h>
 
+#define D_VENDOR_ID         0x08f7
+#define D_PRODUCT_ID        0x0002
+
+static struct usb_device_id g_id_table[] = {
+    {USB_DEVICE(D_VENDOR_ID, D_PRODUCT_ID)},
+    {},
+};
+MODULE_DEVICE_TABLE(usb, g_id_table);
+
+// function for interact to usb device.
+static canusbdriver_probe(struct usb_interface* interface, const struct usb_device_id* id) {
+
+}
+
+static void canusbdriver_disconnect(struct usb_interface* interface) {
+
+}
+
+static struct usb_driver canusbdriver = {
+    .name = "Shymaxtic CAN USB",
+    .probe = canusbdriver_probe,
+    .disconnect = canusbdriver_disconnect,
+    .id_table = g_id_table,
+};
+
+// function for modprobe module.
 static int __init canusbdriver_init(void) {
 
 }
